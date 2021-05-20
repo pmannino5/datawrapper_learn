abad50027d78c015a1fe380f344bc84c4064c2df
pmannino5 <43046160+pmannino5@users.noreply.github.com>
2021-05-20 01:46:04 -0700

    add table code

--- datawrapper_code.R
+++ datawrapper_code.R
@@ -2,7 +2,7 @@
 # Fortunately, there is a nice package called DatawRappr that makes using the API very easy.
 # https://munichrocker.github.io/DatawRappr/index.html
 # For this learning script, I'm going to use the exoneration data I downloaded a couple years ago
-# and make a line graph of exonerations per year
+# and make a line graph of exonerations per year and table of exonerations by race
 # I'll work on additional charts and tables in the future
 
 library(DatawRappr)
@@ -11,12 +11,16 @@ library(dplyr)
 # set working directory
 setwd('/Users/petermannino/Documents/Exoneration')
 
-# import the exoneration dataset
+# download the exoneration dataset
 exon_dat<-readxl::read_excel('publicspreadsheet.xlsx')
 
 # set up connection with datawrapper api 
 datawrapper_auth(api_key="your api key")
+
+
 dw_test_key() # test connection
+
+##### Make a line graph in datawrapper #####
 
 # summarize total exonerations per year
 dw_data<-exon_dat %>%
@@ -58,8 +62,41 @@ dw_publish_chart(chart_id = dw_chart)
 # Delete it
 dw_delete_chart(dw_chart)
 
+##### Make a table in datawrapper #####
 
+# df of exonerations by race
+dw_table_dat<-exon_dat %>%
+  group_by(Race) %>%
+  summarize(Total_Exonerations = n())
 
+dw_table<-dw_create_chart(type='tables') # create empty table
 
+dw_data_to_chart(chart_id = dw_table, x = dw_table_dat) # add data to table
 
+# Add title, annotations, and source to table
+dw_edit_chart(chart_id = dw_table,
+              title = "Exonerations by Race",
+              source_name = "National Exoneration Registry at the University of Michigan",
+              annotate = "Data downloaded in 2019")
 
+# Edit visuals
+dw_edit_chart(chart_id = dw_table,
+              visualize = list(header=list(style=list(bold=TRUE, # bold the header
+                                                      background="#f0f0f0"), # make the background gray
+                                           borderBottom="2px"), # make the line below the header thicker
+                               striped=TRUE, # make the table stripped
+                               sortTable=TRUE, # Sort the table
+                               sortBy = "Total_Exonerations")) # by exonerations
+
+# export table
+table_export<-dw_export_chart(chart_id = dw_table, type=c("png"), plain = FALSE) 
+table_export
+
+# Save the table
+magick::image_write(table_export, "table_export.png")
+
+# publish table online
+dw_publish_chart(dw_table)
+
+# delete table
+dw_delete_chart(chart_id = dw_table)
